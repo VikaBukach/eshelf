7493b5b81b99335c7f35608db704cc7a91de7ac1
Maksym Symonenko <simaxim1@outlook.com>
2024-02-10 20:55:38 +0200

    update index (replaced Provider and BrowserRouter from App.js)

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,5 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Comparing } from "./pages/Comparing";
@@ -14,20 +11,16 @@ import { Notfound } from "./pages/Notfound";
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="/comparing" element={<Comparing />} />
-              <Route path="/favorites" element={<Favorites />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="*" element={<Notfound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/comparing" element={<Comparing />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
