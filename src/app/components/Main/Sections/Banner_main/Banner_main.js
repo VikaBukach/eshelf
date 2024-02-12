8b738f77e5eaca1a87361bfa0933a1b390eb4f51
vikabukach <bukachviko@gmail.com>
2024-02-12 21:30:27 +0200

    fix home page main banner for mobile

--- src/app/components/Main/Sections/Banner_main/Banner_main.js
+++ src/app/components/Main/Sections/Banner_main/Banner_main.js
@@ -4,16 +4,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './Banner_main.scss';
 
-function Arrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", background: "gray" }}
-            onClick={onClick}
-        />
-    );
-}
+// function Arrow(props) {
+//     const { className, style, onClick } = props;
+//     return (
+//         <div
+//             className={className}
+//             style={{ ...style, display: "block", background: "gray" }}
+//             onClick={onClick}
+//         />
+//     );
+// }
 
 function BannerMain(props) {
     var settings = {
@@ -22,8 +22,8 @@ function BannerMain(props) {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 2,
-        nextArrow: <Arrow />,
-        prevArrow: <Arrow />,
+        // nextArrow: <Arrow />,
+        // prevArrow: <Arrow />,
         initialSlide: 0,
         responsive: [
             {
@@ -57,10 +57,24 @@ function BannerMain(props) {
                     <h1 className="banner_main-title">Phantom 4 Pro V2.0</h1>
                     <p className="banner_main-descr">Featuring a 1-inch CMOS sensor that can shoot 4K/60fps videos and
                         20MP photos...</p>
-                    <div className="banner_main-img">
-                        <img
-                             src="/img/kisspng-mavic-pro-dji-phantom-4-pro-dji-phantom-4-pro-unma-dji-phantom-5b51f93fbeb6f3%201.png"
-                             alt="Quadcopter"/>
+                    <div className="banner_main-img-container">
+                        <div>
+                            <svg className="banner_main-svg-l banner_main-svg-activ" width="25" height="25" viewBox="0 0 25 25" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.8752 5.75L8.12524 12.5L14.8752 19.25" stroke="#333333" stroke-opacity="0.2"
+                                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <img className="banner_main-img"
+                            src="/img/kisspng-mavic-pro-dji-phantom-4-pro-dji-phantom-4-pro-unma-dji-phantom-5b51f93fbeb6f3%201.png"
+                            alt="Quadcopter"/>
+                        <div>
+                            <svg className="banner_main-svg-r banner_main-svg-active" width="9" height="17" viewBox="0 0 9 17" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.12476 15.25L7.87476 8.5L1.12476 1.75" stroke="#333333" stroke-opacity="0.2"
+                                      stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
                     </div>
                     <div className="banner_main-price">
                         <div className="banner_main-old-price">₴ 71 999</div>
