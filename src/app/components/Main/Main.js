a0e4bdd9b1cd6f9ffc8a3576b3dd3e35a1086407
vikabukach <bukachviko@gmail.com>
2024-02-12 11:22:27 +0200

    fix size

--- src/app/components/Main/Main.js
+++ src/app/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BannerMain from "./Sections/Banner_main/Banner_main";
 import Top_Brands from "./Sections/Top_Brands/Top_Brands";
+import './Main.scss';
 
 
 const Main = () => {
