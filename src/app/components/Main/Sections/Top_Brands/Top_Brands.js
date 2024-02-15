c3fd5cc7636b1b86b5a879e10375a14d7288de5b
vikabukach <bukachviko@gmail.com>
2024-02-15 14:34:55 +0200

    section top brands for mobile was done

--- src/app/components/Main/Sections/Top_Brands/Top_Brands.js
+++ src/app/components/Main/Sections/Top_Brands/Top_Brands.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import './Top_Brands.scss'
 
 function TopBrands(props) {
     return (
-        <div className="secrion_top-brands">
-            <h6 className="secrion_top-brands-title">
+        <div className="section_top-brands">
+            <h6 className="section_top-brands-title">
                 Top Brands
             </h6>
-            <div className="secrion_top-brands-items">
-            <img className="section_top-brands-logo" src="/img/apple.svg" alt="apple"/>
-            <img className="section_top-brands-logo" src="/img/acer.svg" alt="acer"/>
-            <img className="section_top-brands-logo" src="/img/mi-xiaomi.svg" alt="mi-xiaomi"/>
-            <img className="section_top-brands-logo" src="/img/samsung.svg" alt="samsung"/>
-            <img className="section_top-brands-logo" src="/img/lenovo.svg" alt="lenovo"/>
-            <img className="section_top-brands-logo" src="/img/huawey.svg" alt="huawei"/>
-            <img className="section_top-brands-logo" src="/img/jbl.svg" alt="jbl"/>
-            <img className="section_top-brands-logo" src="/img/hp.svg" alt="hp"/>
+            <div className="section_top-brands-items">
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/apple.svg" alt="apple"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/acer.svg" alt="acer"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/mi-xiaomi.svg" alt="mi-xiaomi"/>
+                </div>
+                <div className="section_top-brands-logo" >
+                    <img className="section_top-brands-logo-img" src="/img/samsung.svg" alt="samsung"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/lenovo.svg" alt="lenovo"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/huawey.svg" alt="huawei"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/jbl.svg" alt="jbl"/>
+                </div>
+                <div className="section_top-brands-logo">
+                    <img className="section_top-brands-logo-img" src="/img/hp.svg" alt="hp"/>
+                </div>
             </div>
-
-
         </div>
     );
 }
