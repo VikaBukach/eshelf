feebc64f40e68b7848d1b37ea9eea1c483528fa9
vikabukach <bukachviko@gmail.com>
2024-02-16 22:27:53 +0200

    section Especially for you for mobile and tablet was done

--- src/app/components/Main/Button/Button.js
+++ src/app/components/Main/Button/Button.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './Button.scss';
 
 function Button({ btnClass, onClick, text }) {
-        return (
-            <Button
-                className={btnClass}
-                onClick={onClick}
-            >
+    return (
+        <button
+            className={btnClass}
+            onClick={onClick}
+        >
+            <div className="btn-text">
                 {text}
-                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="btn-svg" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M5.25 10.2914L12 17.0414L18.75 10.2914" stroke="#8119B1" stroke-width="1.5"
                           stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-            </Button>
-        )
+            </div>
+        </button>
+    )
 }
 
 
--- src/app/components/Main/Sections/Especially_for_you/Especially_for_you.js
+++ src/app/components/Main/Sections/Especially_for_you/Especially_for_you.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../../Button/Button";
+import './Especially_for_you.scss';
 
-function EspeciallyForYou( btnClass, onClick, text ) {
+function EspeciallyForYou(props) {
+    const [ itemsToShow, setItemsToShow] = useState(window.innerWidth >= 768 ? 3 : 2);
+    const totalItems = 10;
+
+
+    const showCards =() => {
+        const additionalItems = window.innerWidth >= 768 ? 3 : 2;
+        if (itemsToShow + additionalItems <= totalItems) {
+            setItemsToShow(itemsToShow + additionalItems);
+        }
+    }
+
+
+
     return (
         <div className="section_especially">
-            <h6 className="ssection_especially-title">
-                Especially for you
-            </h6>
-            <div className="section_especially-products">
-                <div></div>
-                <div></div>
+            <div className="section_especially-wrap">
+                <h6 className="section_especially-title">
+                    Especially for you
+                </h6>
             </div>
-           <Button
-               btnClass="section_especially-products-btn"
-               text={'See more'}
-               // onClick={}
-           />
+
+            <div className="section_especially-products">
+
+                {[...Array(itemsToShow)].map((_, index) => (
+                    <div key={index} className="section-especially-item"></div>
+                ))}
+            </div>
+            { itemsToShow < totalItems && (
+                <Button
+                    btnClass="section_especially-products-btn"
+                    text={'See more'}
+                    onClick={showCards}
+                />
+            )}
         </div>
     );
 }
