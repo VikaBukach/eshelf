95f620d674d2aefb94eb08f04d35480047d59fbb
tusynkabert <138988098+tusynkabert@users.noreply.github.com>
2024-02-21 07:30:40 -0800

    add cart slice

---
+++ src/app/store/slices/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    data: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.data = [...state.data, action.payload];
+    },
+    removeFromCart: (state, action) => {
+      state.data = [...state.data].filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
