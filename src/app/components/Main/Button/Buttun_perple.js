8a9126435cfeaf715486707f0de49e66afc51533
vikabukach <bukachviko@gmail.com>
2024-02-23 12:53:25 +0200

    making banner top for table banner top prod

---
+++ src/app/components/Main/Button/Buttun_perple.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button_perple.scss';
+
+function Button({ btnClass, onClick, text }) {
+    return (
+        <button
+            className={btnClass}
+            onClick={onClick}
+        >
+            <div className="btn-text">
+                {text}
+            </div>
+        </button>
+    )
+}
+
+
+export default Button;
--- src/app/components/Main/Sections/Banner_top-product/Banner_top-product.js
+++ src/app/components/Main/Sections/Banner_top-product/Banner_top-product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "../../Button/Button";
 import "./Banner_top-product.scss";
+import Buttun_perple from "../../Button/Buttun_perple";
 
 function BannerTopProduct(props) {
     return <>
@@ -9,26 +10,28 @@ function BannerTopProduct(props) {
                 Apple Iphone 14
             </h1>
             <img className="banner_top-product-mobile-img" src="/img/Iphone_14.png" alt=""/>
-            <h4 className="banner_top-product-mobile-subtitle">One of the main features of the iPhone 14 was supposed to be
-                    the
-                    absence of bangs, or reduction of them. In version 13...</h4>
-                <Button
-                    btnClass="banner_top-product-mobile-pre-order-btn"
-                    text={'Pre-order'}
-                />
+            <h4 className="banner_top-product-mobile-subtitle">One of the main features of the iPhone 14 was supposed to
+                be
+                the
+                absence of bangs, or reduction of them. In version 13...</h4>
+            <Button
+                btnClass="banner_top-product-mobile-pre-order-btn"
+                text={'Pre-order'}
+            />
         </div>
 
         <div className="banner_top-product-desktop">
-            <img className="banner_top-product-img" src="/img/Iphone_14.png" alt=""/>
-            <div className="banner_top-product-info">
-                <h1 className="banner_top-product-title">
+            <img className="banner_top-product-desktop-img" src="/img/Iphone_14.png" alt=""/>
+            <div className="banner_top-product-desktop-info">
+                <h1 className="banner_top-product-desktop-title">
                     Apple Iphone 14
                 </h1>
-                <h4 className="banner_top-product-subtitle">One of the main features of the iPhone 14 was supposed to be
-                    the
-                    absence of bangs, or reduction of them. In version 13...</h4>
-                <Button
-                    btnClass="banner_top-product-pre-order-btn"
+                <p className="banner_top-product-desktop-descr"> One of the main features of the iPhone 14 was supposed
+                    to be the absence of bangs, or reduction of them. In version 13, Apple narrowed it down a bit, so it
+                    was assumed that next year the company would get rid of the bangs, turning to a "hole" camera
+                    design.</p>
+                <Buttun_perple
+                    btnClass="banner_top-product-desktop-pre-order-btn"
                     text={'Pre-order'}
                 />
             </div>
