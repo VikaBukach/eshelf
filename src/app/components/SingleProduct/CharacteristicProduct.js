42047812d7ae7dcf888a67321194e90fb5073df7
respectket <luxuryua1@gmail.com>
2024-02-29 18:49:36 +0200

    continue characteristic page

--- src/app/components/SingleProduct/CharacteristicProduct.js
+++ src/app/components/SingleProduct/CharacteristicProduct.js
@@ -22,15 +22,206 @@ const CharacteristicProduct = ({product}) => {
                   </dd>
                 </div>
               )}
+              {product.specifications.display.display_resolution && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Display resolution</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.display.display_resolution}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.display.display_matrix_type && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Display matrix type</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.display.display_matrix_type}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.display.frequency && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">The refresh rate of the display is</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.display.frequency}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
 
-              <div className="characteristic-body__item">
-                <dt className="characteristic-body__label">Display resolution</dt>
-                <dd className="characteristic-body__value">
-                  <ul className="characteristic-body__sub-list">
-                    <li>1600x720 (HD+)</li>
-                  </ul>
-                </dd>
-              </div>
+        {product.specifications.operating_system && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Operating system</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.operating_system.type && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Operating system</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.operating_system.type}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
+
+        {product.specifications.processor && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Processor</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.processor.number_of_cores && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">The number of processor cores is</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.processor.number_of_cores}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.processor.type && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Processor frequency</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.processor.type}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
+
+        {product.specifications.memory && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Memory</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.memory.RAM && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">The amount of RAM is</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.memory.RAM}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
+
+        {product.specifications.camera && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Camera</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.camera.number_of_cameras && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Number of cameras</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.camera.number_of_cameras}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.camera.main_camera && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">The maximum resolution of camera is</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.camera.main_camera}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.camera.front_camera && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Front camera</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.camera.front_camera}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.camera.functions && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Camera functions</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      {product.specifications.camera.functions.map((item, i) => (
+                        <li key={i}>{item}</li>
+                      ))}
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
+
+        {product.specifications.battery && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Battery</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.battery.capacity && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Capacity</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.battery.capacity}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.battery.charging_port && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Charging port</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.battery.charging_port}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </section>
+        )}
+
+        {product.specifications.other && (
+          <section className="characteristic-body__group">
+            <h3 className="characteristic-body__heading">Dimensions</h3>
+            <dl className="characteristic-body__list">
+              {product.specifications.other.dimension && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Dimensions (HxWxD)</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.other.dimension}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {product.specifications.other.weight && (
+                <div className="characteristic-body__item">
+                  <dt className="characteristic-body__label">Body weight</dt>
+                  <dd className="characteristic-body__value">
+                    <ul className="characteristic-body__sub-list">
+                      <li>{product.specifications.other.weight}</li>
+                    </ul>
+                  </dd>
+                </div>
+              )}
             </dl>
           </section>
         )}
--- src/app/pages/product/ProductPage.js
+++ src/app/pages/product/ProductPage.js
@@ -23,6 +23,39 @@ const ProductPage = () => {
     dispatch(fetchDataOfProducts(`${collection}`));
   }, [dispatch, collection]);
 
+  useEffect(() => {
+    // Восстановление выбранного таба из URL при загрузке страницы
+    const params = new URLSearchParams(window.location.search);
+    const tab = params.get("tab");
+    if (tab) {
+      dispatch(changeTabs(tab));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    // Восстановление выбранного таба из localStorage при загрузке страницы
+    const storedTab = localStorage.getItem("selectedTab");
+    if (storedTab) {
+      dispatch(changeTabs(storedTab));
+    }
+  }, [dispatch]);
+
+  const handleTabClick = (tabName) => {
+    // Обновление хранилища и состояния таба при клике на табе
+    localStorage.setItem("selectedTab", tabName);
+
+    // Обновление URL при клике на табе
+    const url = new URL(window.location.href);
+    if (tabName === "About the product") {
+      url.searchParams.delete("tab");
+    } else {
+      url.searchParams.set("tab", tabName.toLowerCase().replace(/ /g, "-"));
+    }
+    window.history.pushState({ path: url.href }, "", url.href);
+
+    dispatch(changeTabs(tabName));
+  };
+
   if (!product) {
     return <div>Loading...</div>;
   }
@@ -39,7 +72,8 @@ const ProductPage = () => {
               <li
                 className={`product-details__tabs-item ${tabs === item ? "product-details__tabs-active" : ""}`}
                 key={id}
-                onClick={() => dispatch(changeTabs(item))}
+                //onClick={() => dispatch(changeTabs(item))}
+                onClick={() => handleTabClick(item)}
               >
                 {item}
               </li>
