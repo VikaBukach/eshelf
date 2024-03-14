c8ab90f947675229c1bd62a9509b943d894da1d9
vikabukach <bukachviko@gmail.com>
2024-03-14 09:13:06 +0200

    added label in ContactDetails.js

--- src/app/components/OrderPage/components/ContactDetails.js
+++ src/app/components/OrderPage/components/ContactDetails.js
@@ -2,7 +2,7 @@ import "../Order.scss";
 import InputMask from "../../ui/Input/InputMask";
 import { validateEmail } from "..";
 
-export const ContactDetails = ({ state, setState }) => {
+export const ContactDetails = ({ state, setState, accountSettingsClass }) => {
   const inputs = [
     {
       key: "surname",
@@ -32,17 +32,20 @@ export const ContactDetails = ({ state, setState }) => {
   ];
 
   return (//added class ${accountSettingsClass} for overriting styles in AccountSettings
-    <div className={`${"orderPage__contactDetails"} ${accountSettingsClass}`}>
-      {inputs.map((input) => (
-        <InputMask
-          validateFunction={input?.validateFunction}
-          key={input.key}
-          input={input}
-          setState={setState}
-          state={state}
-        />
-      ))}
-    </div>
+      <div className={`${"orderPage__contactDetails"} ${accountSettingsClass}`}>
+        {inputs.map((input) => (
+            <div className="inputContainer" key={input.key}>
+              <label htmlFor={input.key}>{input.label}</label>
+              <InputMask
+                  id={input.key}
+                  validateFunction={input?.validateFunction}
+                  input={input}
+                  setState={setState}
+                  state={state}
+              />
+            </div>
+        ))}
+      </div>
   );
 };
 
