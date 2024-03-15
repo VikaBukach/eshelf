e4027fa55588a540ea96db30d01f740da056e227
vikabukach <bukachviko@gmail.com>
2024-03-15 11:52:47 +0200

    doing delivere

--- src/app/components/UserProfile/Delivery/WarrantyDelivery.js
+++ src/app/components/UserProfile/Delivery/WarrantyDelivery.js
@@ -3,18 +3,41 @@ import React, {useState, useEffect, useRef} from 'react';
 
 
 const WarrantyDelivery = () => {
+    const [toggle, setToggle] = useState(false);
+    const [heightEl, setHeightEl] = useState();
+    const refHeight = useRef(null);
+
+    useEffect(() => {
+        if(refHeight.current){
+            setHeightEl(`${refHeight.current.scrollHeight}px`)
+        }
+    }, [])
+
+    const toggleState = () => {
+        setToggle(!toggle)
+    }
 
     return (
+        <div className="delivery-to-user-content">
         <div className="delivery-to-user">
-            <button className="delivery-to-user-btn-visible">
+            <button
+                onClick={toggleState}
+                className="delivery-to-user-btn-visible">
                 <span>How to place an order?</span>
 
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                    width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.625 18.75L16.375 12L9.625 5.25" stroke="#333333" strokeWidth="0.872238"
                           strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
+
             </button>
-            <div className="delivery-to-user-btn-toggle">
+            <div
+                className={toggle ? "delivery-to-user-btn-toggle animated" : "delivery-to-user-btn-toggle"}
+                style={{height: toggle ? `${heightEl}` : "0px"}}
+                ref={refHeight}
+            >
+                <div aria-hidden={toggle ? "true" : "false"}>
                 <p>You can place an order on the site allo.ua around the clock.
                 </p>
                 <p>We will not disturb you and make phone calls if you do not wish to contact us.
@@ -51,11 +74,12 @@ const WarrantyDelivery = () => {
                 </ul>
                 <p>For one product from the categories of medium or small household appliances, costing up to 10,000
                     UAH. and weighing up to 20 kg</p>
-
+                </div>
 
             </div>
 
 
+        </div>
         </div>
     );
 };
