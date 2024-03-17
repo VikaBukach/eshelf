75812ba61649873f6db000d8f6974d23e6f3f9cf
respectket <luxuryua1@gmail.com>
2024-03-17 19:46:43 +0200

    develop footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,11 +1,30 @@
 import React from "react";
 
+import { ReactComponent as PaymentPay } from "../../../assets/images/apple-pay.svg";
+import { ReactComponent as PaymentVisa } from "../../../assets/images/visa.svg";
+import { ReactComponent as PaymentMasterCard } from "../../../assets/images/mastercard.svg";
+
 const Footer = () => {
   return (
     <>
-      <div className="footer">
-        <footer className="footer"> &copy; All rights reserved REGIMENT TRADE LLC, 2010–2022</footer>;
-      </div>
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__wrapper">
+            <div className="footer__copyright">&copy; <span>All rights reserved</span> REGIMENT TRADE LLC, 2010–2024</div>
+            <div className="footer__payments">
+              <div className="footer__payments-item">
+                <PaymentPay />
+              </div>
+              <div className="footer__payments-item">
+                <PaymentVisa />
+              </div>
+              <div className="footer__payments-item">
+                <PaymentMasterCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
